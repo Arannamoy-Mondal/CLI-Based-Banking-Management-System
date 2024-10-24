d69ffe40fbdc59c3ec2c16d86e9d6ba023b8e317
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-10-24 15:07:21 +0600

    Admin

--- Bank.py
+++ Bank.py
@@ -1,13 +1,13 @@
 from Account import Account
 class Bank:
     def __init__(self):
-        self.AccountList={}
+        self.__AccountList={}
         self.__totalBalance=0
         self.__totalLoanAmount=0
         self.__loanFeature=True
     def createAcc(self,name,email,address,acType):
         account=Account(name,email,address,acType)
-        self.AccountList[account.acNumber]=account
+        self.__AccountList[account.acNumber]=account
     def deposit(self,x):
         self.__totalBalance+=x
     def withdraw(self,x):
@@ -15,11 +15,21 @@ class Bank:
     def takeLoan(self,x):
         self.__totalLoanAmount+=x
     def loanStatus(self,x):
-        self.__loanFeature=x    
+        self.__loanFeature=x  
+    @property 
+    def checkLoanStatus(self):
+        if self.__loanFeature==True:
+           print(f"Loan Status of Bank: ON")
+        else:
+            print(f"Loan Status of Bank: OFF")
     @property
     def totalBalance(self):
         print(f"Total Balance: {self.__totalBalance}")
+    @property
+    def totalLoanAmount(self):
+        print(f"Total loan amount: {self.__totalLoanAmount}")
+    @property
     def accList(self):
-        for key,value in self.AccountList.items():
+        for key,value in self.__AccountList.items():
             print(f"{value.name} {value.email} {value.address} {value.acType} {key}")
 
--- main.py
+++ main.py
@@ -1,9 +1,61 @@
 from Bank import Bank
+from Account import Account
 admin=Bank()
-admin.createAcc('XYZ','XYZ','XYZ','Savings')
-admin.createAcc('XYZ','XYZ','XYZ','Savings')
-admin.createAcc('XYZ','XYZ','XYZ','Current')
-admin.createAcc('XYZ','XYZ','XYZ','Savings')
-admin.createAcc('XYZ','XYZ','XYZ','Savings')
-admin.createAcc('XYZ','XYZ','XYZ','Savings')
-admin.accList()
+
+def adminFeatures(option):
+    if option==1:
+        name=input("Enter account owner name:")
+        email=input("Enter account owner email:")
+        address=input("Enter account owner address:")
+        accType=input("Enter account type (Savings/Current):")
+        if accType.lower()=="savings" or accType.lower()=="current":
+            admin.createAcc(name,email,address,accType)
+        else:
+            accType=input("Please correct account type. If you want to cancel the process enter E. OtherWise accountype")
+            if accType.lower()=="savings" or accType.lower()=="current":
+                admin.createAcc(name,email,address,accType)
+            else:
+                return
+    elif option==2:
+        pass
+    elif option==3:
+        admin.accList
+    elif option==4:
+        admin.totalBalance
+    elif option==5:
+        admin.totalLoanAmount
+    elif option==6:
+        admin.checkLoanStatus
+        x=input("Loan feature (ON/OFF):")
+        if x.lower()=="on":
+            admin.loanStatus("True")
+            admin.checkLoanStatus
+        elif x.lower()=="off":
+            admin.loanStatus("False")
+            admin.checkLoanStatus
+        else:
+            print("Invalid option for loan status")
+
+
+
+while True:
+    userType=input("U for User, A for Admin, E for exit:")
+    if userType.lower()=='a':
+        while True:
+            print("0 for exit.\n1 for creat an account.\n2 for delete user account.\n3 see all user accounts list.\n4 "+
+              "total available balance of the bank.\n5 total loan amount.\n6 for on or off the loan feature of the bank.")
+            option=input("Option:")
+            if option=="0":
+                break
+            elif option == "1" or option == "2" or option == "3" or option == "4" or option == "5" or option == "6":
+               adminFeatures(int(option))
+            else:
+                print("Invalid input")
+    elif userType.lower()=='u':
+        print("Welcome to our bank")
+    elif userType.lower()=='e':
+        print("Thanks for visiting")
+        break
+    else:
+        print("You have enter invalid option")
+        
